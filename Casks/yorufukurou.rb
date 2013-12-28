65b1fa408c887ccc2321eb8aea207d09e7ee7e97
chiastolite <chiastolite.1980@gmail.com>
2013-12-29 04:06:31 +0900

    add Yorufukurou

---
+++ Casks/yorufukurou.rb
@@ -0,0 +1,7 @@
+class Yorufukurou < Cask
+  url 'http://aki-null.net/yf/YoruFukurou_SL.zip'
+  homepage 'https://sites.google.com/site/yorufukurou/'
+  version '2.79'
+  sha1 '2f1e94adfd39ab0e43ba5e4e22609357e4987a5c'
+  link 'Yorufukurou.app'
+end
